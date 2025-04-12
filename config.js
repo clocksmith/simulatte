7e1c92cc9a3b3e7d3e4754aec764d620d406df40
XY Z <xyz@mac.lan>
2025-04-12 19:49:58 -0400

    .

--- config.js
+++
@@ -1,7 +0,0 @@
-const APP_CONFIG = {
-  API_KEY: "AIzaSyBGp07O7d-zcP350i6aLD1aOqK8XORJzwQ",
-  PROJECT_ID: "LSD1729",
-  BASE_GEMINI_MODEL: "gemini-2.0-flash-001",
-  ADVANCED_GEMINI_MODEL: "gemini-2.5-pro-exp-03-25",
-  GEMINI_MODEL_OPTIMIZER: "model-optimizer-exp-04-09",
-};
