fb28024d58f929b27bd03d06c90812fc98dfc586
X <xyz@mac.lan>
2025-04-12 21:52:32 -0400

    .

--- fake_config.js
+++ fake_config.js
@@ -10,7 +10,7 @@
 const APP_CONFIG = {
   API_KEY: "<nope>",
   PROJECT_ID: "<nope>",
-  BASE_GEMINI_MODEL: "gemini-2.0-flash-001",
+  BASE_GEMINI_MODEL: "gemini-2.0-flash-thinking-exp-01-21",
   ADVANCED_GEMINI_MODEL: "gemini-2.5-pro-exp-03-25",
   GEMINI_MODEL_OPTIMIZER: "model-optimizer-exp-04-09",
 };
